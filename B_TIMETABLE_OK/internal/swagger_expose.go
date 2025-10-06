f4a12636f1507d50819bdccc0429d607074417fc
RandomUserLt <random1004m@gmail.com>
2025-10-06 02:58:03 +0200

    Exposition swagger pour l'API timetable

---
+++ B_TIMETABLE_OK/internal/swagger_expose.go
@@ -0,0 +1,9 @@
+package internal
+
+// @title        Unicorn – Config API
+// @version      1.0
+// @description  API de configuration : agendas & alerts (CRUD).
+// @schemes      http
+// @BasePath     /
+// @contact.name Morty
+// @contact.url  http://localhost:8080/swagger/index.html
